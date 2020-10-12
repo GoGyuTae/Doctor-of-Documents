ac037feabe3a10638255020959ff7fca0993c5ff
GoGyuTae <rbxo8806@naver.com>
2020-10-12 17:38:07 +0900

    foundation structure

---
+++ app.js
@@ -0,0 +1,79 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const { sequelize } = require('./models/index.js');
+const app = express();
+const models = require('./models');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+//model.sync();
+
+models.sequelize.sync().then( () => {
+  console.log(" DB 연결 성공");
+}).catch(err => {
+  console.log("연결 실패");
+  console.log(err);
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!\n');
+  
+ });
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+
+app.post('/postData', async(req, res) => {
+  
+
+  
+  try{
+    const user = await models.user.create({ 
+      useremail : req.body.useremail, 
+      name : req.body.name,
+     });
+
+    console.log('succcess', user.toJSON());
+    
+    res.json(
+      {
+      useremail : user.useremail
+    });
+
+  } catch (err) {
+    console.log(err, req.body.useremail);
+  }
+
+
+
+
+
+/*
+  models.user.create({
+    useremail : req.body.useremail,
+    name : req.body.name,
+
+  })
+  
+  .then((user) => {
+    console.log('success postData', user.toJSON());
+
+    res.json(
+      {
+      useremail : user.useremail
+    });
+  })
+  .catch((err) => {
+    console.log(err, req.body.useremail);
+    
+
+  })
+*/
+
+
+});
+
---
+++ models/user.js
@@ -0,0 +1,20 @@
+/*const { DataTypes } = require("sequelize/types");
+const { sequelize } = require(".");
+*/
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('user', {
+        useremail: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            unique: true,
+        },
+
+        name: {
+            type: DataTypes.STRING(10),
+            allowNull: false,
+        },
+
+    },{
+        timestamps: true,
+    });
+}
