df61af3231f4640292be040f5eef01a866c2db16
GoGyuTae <rbxo8806@naver.com>
2020-10-19 17:11:59 +0900

    Add password encryption with bcrypt

--- routes/controllers/signupController.js
+++ routes/controllers/signupController.js
@@ -1,32 +1,53 @@
 const Models = require('../../models/');
+const bcrypt = require('bcrypt');
 
 module.exports = {
+    
     joinUser: (req, res) => {
-        console.log(req.body);
-        
-        Models.usertable.create ({ 
-            email : req.body.email, 
-            name : req.body.name,
-            password : req.body.password
-        })
-        .then((result) => {
-            console.log('success join', result.toJSON());
-            console.log(result);
-
-        })
-        .catch((err) => {
-
-            // 이메일 형식이 아닌경우
-            if (err.name === 'SequelizeValidationError') {  
-                return res.status(400).json({warn: 'check the email pattern'});
-            }
-            // 이미 가입된 이메일인 경우
-            if (err.name === 'SequelizeUniqueConstraintError') {
-                return res.status(400).json({warn: 'Already join email'});
+        let {email, name, password} = req.body;
+        bcrypt.genSalt(10, function(err, salt) {
+            if(err) {
+                console.log('bcrypt.genSalt() errer : ', err.message);
+            } else {
+                bcrypt.hash(password, salt, null, function(err, hash) {
+                    if(err) {console.log('bcrypt.hash() errer : ', err.message);}
+                             
+                })
+                .then((result) =>{
+                    password = result;
+                    console.log(password);
+                    Models.usertable.create ({ 
+                        email : email, 
+                        name : name,
+                        password : password
+                    })
+                    .then((result) => {
+                        console.log('success join', result.toJSON());
+                        res.status(201).json(result);
+                    })
+                    .catch((err) => {
+            
+                        // 이메일 형식이 아닌경우 (클라이언트 오류 (4xx))
+                        if (err.name === 'SequelizeValidationError') {  
+                            return res.status(400).json({warn: 'check the email pattern'});
+                        }
+                        // 이미 가입된 이메일인 경우 
+                        if (err.name === 'SequelizeUniqueConstraintError') {
+                            return res.status(400).json({warn: 'Already join email'});
+                        }
+                        
+                        // 서버가 처리 하지 못하는 경우 (5xx)
+                        res.status(500).json({error: err});
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                
             }
             
-            res.status(500).json({error: err});
-        });
+        })
+        
     },
 
     updateUser: (req, res) => {
